60243ad948e9294c6c83cc837aca61a24c578def
Matt Tedder <mjtedder@icloud.com>
2021-10-12 17:24:06 -0600

    Removes default scaffold, adds about me section

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Landing msg="Matt Tedder"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Landing
   }
 }
 </script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="landing">
+    <h1>{{ msg }}</h1>
+    
+    <section class="about">
+      <h2>About Me</h2>
+        <ul>
+          <li>Avid Traveler</li>
+          <li>Based out of Denver, Colorado, USA</li>
+          <li>Enjoys
+            <span class="emoji">{{ randomEmoji }}</span>
+          </li>
+        </ul>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      emojis: ["🍕", "🎸", "🎮"],
+      randomEmoji: ""
+    }
+  },
+  mounted() {
+    this.randomize();
+  },
+  methods: {
+    randomize() {
+      this.randomEmoji = this.emojis[Math.floor(Math.random() * this.emojis.length)];
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
